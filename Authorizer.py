c06024dddd300b1fa3a5966f9a5f36c4b0713dce
aussiearef <arefkr@gmail.com>
2023-06-20 06:12:17 +1000

    Pushed Authorizer

--- Authorizer.py
+++ Authorizer.py
@@ -1,95 +1,67 @@
-using System.IdentityModel.Tokens.Jwt;
-using System.Text.Json;
-using Amazon.Lambda.APIGatewayEvents;
-using Amazon.Lambda.Core;
-using Amazon.SecretsManager;
-using Amazon.SecretsManager.Model;
-using Microsoft.IdentityModel.Tokens;
+import json
+from typing import List, Dict, Any
+import boto3
+import jwt
+from jwt import PyJWK
 
-[assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
-namespace LambdaAuthorizer;
+def auth(event, context):
+    id_token = event['queryStringParameters']['token']
 
-public class Authorizer
-{
-    public async Task<APIGatewayCustomAuthorizerResponse> Auth(APIGatewayCustomAuthorizerRequest request)
-    {
-        
-        
-        var idToken = request.QueryStringParameters["token"];
-        Console.WriteLine($"Token is {idToken}");
-        var idTokenDetails = new JwtSecurityToken(idToken);
-
-        var kid = idTokenDetails.Header["kid"].ToString();
-        var issuer = idTokenDetails.Claims.First(x => x.Type == "iss").Value;
-        var audience = idTokenDetails.Claims.First(x => x.Type == "aud").Value;
-        var userId = idTokenDetails.Claims.First(x => x.Type == "sub").Value;
-        
-        var response = new APIGatewayCustomAuthorizerResponse()
-        {
-            PrincipalID = userId,
-            PolicyDocument = new APIGatewayCustomAuthorizerPolicy()
-            {
-                Version = "2012-10-17",
-                Statement = new List<APIGatewayCustomAuthorizerPolicy.IAMPolicyStatement>()
+    id_token_details = jwt.decode(id_token, options={"verify_signature": False})
+    id_token_header = jwt.get_unverified_header(id_token)
+    
+    
+    kid = id_token_header['kid']
+    alg = id_token_header['alg']
+    
+    issuer = id_token_details['iss']
+    audience = id_token_details['aud']
+    user_id = id_token_details['sub']
+    
+    response = {
+        "principalId": user_id,
+        "policyDocument": {
+            "Version": "2012-10-17",
+            "Statement": [
                 {
-                    new APIGatewayCustomAuthorizerPolicy.IAMPolicyStatement()
-                    {
-                        Action = new HashSet<string>(){"execute-api:Invoke"},
-                        Effect = "Allow",
-                        Resource = new HashSet<string>(){request.MethodArn}
-                    }
+                    "Action": "execute-api:Invoke",
+                    "Effect": "Allow",
+                    "Resource": event['methodArn']
                 }
-            }
-        };
-        
-
-        var secretsClient = new AmazonSecretsManagerClient();
-        var secret = await secretsClient.GetSecretValueAsync(new GetSecretValueRequest
-        {
-            SecretId = "hotelCognitoKey"
-        });
-
-        var privateKeys = secret.SecretString;
-
-        Console.WriteLine($"JWKS set: {privateKeys}");
-        var jwks = new JsonWebKeySet(privateKeys);
-        
-        foreach (var key in jwks.Keys)
-        {
-            Console.WriteLine(key.Kid);
+            ]
         }
-        var privateKey = jwks.Keys.First(x => x.Kid == kid);
-
-        var handler = new JwtSecurityTokenHandler();
-        var result = await handler.ValidateTokenAsync(idToken, new TokenValidationParameters
-        {
-            ValidIssuer = issuer,
-            ValidAudience = audience,
-            IssuerSigningKey = privateKey
-        });
-
-        if (!result.IsValid) throw new UnauthorizedAccessException("Token not valid");
-
-        var apiGroupMapping = new Dictionary<string, string>()
-        {
-            {"listadminshotel+", "Admin"},
-            {"admin+", "Admin"}
-        };
-
-        var expectdGroup = apiGroupMapping.FirstOrDefault(x =>
-            request.Path.Contains(x.Key, StringComparison.InvariantCultureIgnoreCase));
-
-        if (!expectdGroup.Equals(default(KeyValuePair<string, string>)))
-        {
-            var userGroup = idTokenDetails.Claims.First(x => x.Type == "cognito:groups").Value;
-            if (string.Compare(userGroup , expectdGroup.Value, 
-                    StringComparison.InvariantCultureIgnoreCase) != 0)
-            {
-                response.PolicyDocument.Statement[0].Effect = "Deny";
-            }
-        }
-        
-        return response;
     }
-}
+    
+    secrets_client = boto3.client('secretsmanager')
+
+    secret = secrets_client.get_secret_value(SecretId="hotelCognitoKey")
+    secret_keys = secret['SecretString']
+    
+    jwks = json.loads(secret_keys)
+    
+
+    public_key = next(filter(lambda x: x['kid'] == kid, jwks['keys']))
+    jwtKey = PyJWK.from_dict(public_key)
+
+    decoded = jwt.decode(
+            id_token, 
+            jwtKey.key, 
+            algorithms=[alg], 
+            issuer=issuer, 
+            audience=audience
+        )
+    
+    api_group_mapping = {
+        "listadminhotel+": "Admin",
+        "admin+": "Admin"
+    }
+    
+    expected_group = next((v for k, v in api_group_mapping.items() if k in event['path']), None)
+    
+    if expected_group:
+        user_group = id_token_details.get('cognito:groups', None)
+        if user_group and expected_group not in user_group:
+            response['policyDocument']['Statement'][0]['Effect'] = "Deny"
+    
+    return response
